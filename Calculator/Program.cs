a4b6f84043ae7a9c6b2f0d9a15b8e3337d6e3bcd
Eivind Egholm Vørmadal <eev_98@hotmail.com>
2022-04-13 09:50:20 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using Calculator;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var calc = new Calculator.Calculator();
+
+
+Console.WriteLine("enter number A");
+string num1;
+double numA;
+bool isDouble1;
+
+
+do
+{
+    num1 = Console.ReadLine();
+
+    isDouble1 = double.TryParse(num1, out numA);
+
+    if (!isDouble1)
+    {
+        Console.WriteLine("Not valid input. Try Again:");
+    }
+} while (!isDouble1);
+
+
+string num2;
+double numB;
+bool isDouble2;
+
+
+Console.WriteLine("enter number B");
+
+
+
+do
+{
+    num2 = Console.ReadLine();
+
+    isDouble2 = double.TryParse(num2, out numB);
+
+    if (!isDouble2)
+    {
+        Console.WriteLine("Not valid input. Try Again:");
+    }
+} while (!isDouble2);
+
+
+
+var result = calc.Add(numA, numB);
+
+Console.WriteLine($"Result is {result}");
+Console.ReadLine();
